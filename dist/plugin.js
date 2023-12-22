a6d0bd51e461bd0790dcbbee8fdea11afa40c1b2
Win_7 <90262580+W-i-n-7@users.noreply.github.com>
2023-12-22 14:48:20 +0200

    Update plugin.js

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,5 +1,5 @@
-exports.version = 1.0
+exports.version = 1.1
 exports.description = "To set a wallpaper go in to plugins/wallpaper-plugin/public/wallpaper.png"
-exports.apiRequired = 1
+exports.apiRequired = 3
 exports.repo = "W-i-n-7/wallpaper-plugin"
 exports.frontend_css = 'style.css'
